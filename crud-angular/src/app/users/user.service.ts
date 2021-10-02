f5f09bb87d4a9cb91d6c9049733e3729b81cb569
igorbock <igorbdaluz@hotmail.com>
2021-10-02 17:22:23 -0300

    primeira aula

---
+++ crud-angular/src/app/users/user.service.ts
@@ -0,0 +1,18 @@
+import { ResponseUsers } from './user.model';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private url = "https://reqres.in/api/users?page=2";
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(): Observable<ResponseUsers>{
+    return this.http.get<ResponseUsers>(this.url);
+  }
+}
